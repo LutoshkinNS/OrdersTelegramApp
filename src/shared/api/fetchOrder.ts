f7ba03536f23ad196bbb88c904700e4f81bf9515
Nikita Lutoshkin <n28280202@gmail.com>
2025-03-05 23:03:17 +0300

    add error handling

--- src/shared/api/fetchOrder.ts
+++ src/shared/api/fetchOrder.ts
@@ -1,17 +1,66 @@
 import {FETCH_URL} from "./config.ts";
-import {OrderType} from "../../pages/Main.tsx";
+
+export interface ProductType {
+    id: number;
+    label: string;
+    price: number;
+    count: number;
+}
+
+export interface StatusType {
+    status: string;
+    date: string;
+}
+
+export interface StatusesType {
+    currentStatus: string;
+    historyStatuses: Array<StatusType>;
+}
+
+export interface OrderType {
+    trackNumber: string;
+    customer: string;
+    totalValue: number;
+    products: Array<ProductType>;
+    description: string | null;
+    statuses: StatusesType;
+}
+
+export interface ErrorType {
+    code: string
+    message: string;
+}
 
 export const fetchOrder = async (trackNumber: string): Promise<OrderType> => {
     try {
-        const response = await fetch(`${FETCH_URL}/api/order?trackNumber=${trackNumber}`);
+        console.log('Fetching order from:', `${FETCH_URL}/api/order?trackNumber=${trackNumber}`);
+        
+        const response = await fetch(`${FETCH_URL}/api/order?trackNumber=${trackNumber}`, {
+            method: 'GET',
+            // headers: {
+            //     'Accept': 'application/json',
+            //     'Content-Type': 'application/json',
+            // },
+            // mode: 'cors',
+            // credentials: 'include'
+        });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error("Непредвиденная ошибка при запросе данных");
         }
 
-        return await response.json();
+        const data = await response.json();
+        return data;
     } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error
+        if (error instanceof Error) {           
+            if (error.message === 'Failed to fetch') {
+                throw new Error('Не удалось подключиться к серверу. Попробуйте позже.');
+            }
+            
+            throw error;
+        } else {
+            console.error('fetchOrder unknown error:', error);
+            throw new Error('Неизвестная ошибка при запросе данных');
+        }
     }
 };
